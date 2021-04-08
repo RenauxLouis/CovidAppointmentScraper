465211f31da501737f129bc93c252ba1dd1164e5
RenauxLouis <33155349+RenauxLouis@users.noreply.github.com>
2021-04-07 22:13:09 -0400

    Update cron_job.sh

--- cron_job.sh
+++ cron_job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export PASSWORD="ALSgYT7Y73kvUtE"
+export PASSWORD="your_password"
 export PATH=$PATH:/usr/local/bin && /usr/local/bin/python3 /Users/louisrenaux/CovidAppointmentScraper/scrape_covid_appointment_page.py >> ~/cron.log 2>&1
