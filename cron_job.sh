9116b0b88a77f20ab87c0125fb060a7d1df96566
RenauxLouis <renauxlouis@gmail.com>
2021-04-07 22:12:12 -0400

    add cron job

---
+++ cron_job.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PASSWORD="ALSgYT7Y73kvUtE"
+export PATH=$PATH:/usr/local/bin && /usr/local/bin/python3 /Users/louisrenaux/CovidAppointmentScraper/scrape_covid_appointment_page.py >> ~/cron.log 2>&1
